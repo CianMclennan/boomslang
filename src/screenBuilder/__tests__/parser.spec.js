5e78b450b7faf72ffd3b162436e8b133ff6ef750
Cian Kinsella McLennan <ciankm88@gmail.com>
2021-09-18 21:45:39 +0100

    fixed tests

--- src/screenBuilder/__tests__/parser.spec.js
+++ src/screenBuilder/__tests__/parser.spec.js
@@ -4,9 +4,9 @@ import parse from '../defaultParser.js';
 describe('Component Parser', () => {
 	it('Should return the value that is passed into it when it has no component attribute.', () => {
 		let value = parse('string');
-		expect(value).toBe('string');
+		expect(value).toBe('"string"');
 		value = parse(2);
-		expect(value).toBe(2);
+		expect(value).toBe('2');
 	});
 	it('Should return a react component if passed an object with valid component attribute', () => {
 		const value = parse(data);
