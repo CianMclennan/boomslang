8fb5fe72d4254fe19dfcde424c0c6d1d800a50d3
Cian Kinsella McLennan <ciankm88@gmail.com>
2023-07-11 19:50:47 +0100

    update

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,43 +1,48 @@
-const path = require('path');
-const settings = require('./build-settings');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+	const path = require('path');
+	const settings = require('./build-settings');
+	const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-module.exports = () => {
-	return {
-		entry: {
-			boomslang: path.resolve(
-				__dirname,
-				'src',
-				settings.editor ? 'editor/editor.js' : 'index.js'
-			),
-		},
-		module: {
-			rules: [
-				{
-					test: /\.s*css$/,
-					use: ['style-loader', 'css-loader', 'sass-loader'],
-				},
-				{
-					test: /\.jsx*$/,
-					exclude: /node_modules/,
-					use: ['babel-loader', 'eslint-loader'],
-				},
-				{
-					test: /\.svg$/,
-					use: 'file-loader',
-				},
-			],
-		},
-		plugins: [
-			new HtmlWebpackPlugin({
-				template: path.resolve(__dirname, 'src', 'index.html'),
-			}),
-		],
-		resolve: {
-			alias: {
-				src: path.resolve(__dirname, 'src'),
+	module.exports = () => {
+		return {
+			entry: {
+				boomslang: path.resolve(
+					__dirname,
+					'src',
+					settings.editor ? 'editor/editor.js' : 'index.js'
+				),
 			},
-			extensions: ['.jsx', '.js'],
-		},
+			module: {
+				rules: [
+					{
+						test: /\.s*css$/,
+						use: ['style-loader', 'css-loader', 'sass-loader'],
+					},
+					{
+						test: /\.jsx*$/,
+						exclude: /node_modules/,
+						use: ['babel-loader', 'eslint-loader'],
+					},
+					{
+						test: /\.svg$/,
+						use: 'file-loader',
+					},
+				],
+			},
+			plugins: [
+				new HtmlWebpackPlugin({
+					template: path.resolve(__dirname, 'src', 'index.html'),
+				}),
+			],
+			resolve: {
+				alias: {
+					src: path.resolve(__dirname, 'src'),
+				},
+				extensions: ['.jsx', '.js'],
+			},
+			mode: 'development',
+			performance: {
+				hints: false,
+			},
+			devtool: 'source-map',
+		};
 	};
-};
